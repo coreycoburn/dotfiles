c9fa2c5c46d4f167b141f76a384a219c70be1ee3
Corey Coburn <corey@coburn.codes>
2023-02-17 23:10:34 -0800

    Update aliases for work

--- aliases.zsh
+++ aliases.zsh
@@ -1,145 +1,51 @@
-function php() {
-  if [[ $(basename $(pwd)) == "dash" ]]; then
-    docker-compose exec api php $1 $2 $3
-  fi
-  if [[ $(basename $(pwd)) == "dash-api" ]]; then
-    docker-compose --file ../docker-compose.yml exec api php $1 $2 $3
-  fi
-}
-
-function art() {
-  if [[ $(basename $(pwd)) == "dash" ]]; then
-    docker-compose exec api php artisan $1 $2 $3
-  fi
-  if [[ $(basename $(pwd)) == "dash-api" ]]; then
-    docker-compose --file ../docker-compose.yml exec api php artisan $1 $2 $3
-  fi
-}
-
-function composer() {
-  if [[ $(basename $(pwd)) == "dash" ]]; then
-    docker-compose exec api composer $1 $2 $3
-    docker cp $(docker-compose ps -q api):/var/www/html/vendor dash-api
-  fi
-  if [[ $(basename $(pwd)) == "dash-api" ]]; then
-    docker-compose --file ../docker-compose.yml exec api composer $1 $2 $3
-    docker cp $(docker-compose --file ../docker-compose.yml ps -q api):/var/www/html/vendor .
-  fi
-}
-
-function xon() {
-  if [[ $(basename $(pwd)) == "dash" ]]; then
-    sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=debug,develop/g" .docker/env/.env.api
-    docker-compose up -d api
-  fi
-  if [[ $(basename $(pwd)) == "dash-api" ]]; then
-    sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=debug,develop/g" ../.docker/env/.env.api
-    docker-compose --file ../docker-compose.yml up -d api
-  fi
-}
-
-function xoff() {
-  if [[ $(basename $(pwd)) == "dash" ]]; then
-    sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=off/g" .docker/env/.env.api
-    docker-compose up -d api
-  fi
-  if [[ $(basename $(pwd)) == "dash-api" ]]; then
-    sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=off/g" ../.docker/env/.env.api
-    docker-compose --file ../docker-compose.yml up -d api
-  fi
-}
-
+###########################################################
 # Shortcuts
+###########################################################
+
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias src="source $HOME/.zshrc"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
+###########################################################
 # Directories
+###########################################################
+
 alias dotfiles="pstorm $DOTFILES"
 alias zshconfig="pstorm $HOME/.zshrc"
 alias library="cd $HOME/Library"
 alias dev="cd $HOME/Dev"
 
-# Laravel
-alias seed="php artisan db:seed"
-
-# PHP
-alias cfresh="rm -rf vendor/ composer.lock && composer i"
-
-# JS
-alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-alias watch="npm run watch"
-
-# Composer
-alias ci="composer install"
-alias cim="COMPOSER_MEMORY_LIMIT=-1 ci"
-
-# Switching branches
-alias switch="ci && npm install && npm run dev"
-
+###########################################################
 # Docker
+###########################################################
+
 alias docker-composer="docker-compose"
 
-# Database
-function db() {
-  [ ! -f .env ] && {
-    echo "No .env file found."
-    exit 1
-  }
-
-  DB_CONNECTION=$(grep DB_CONNECTION .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-  DB_HOST=$(grep DB_HOST .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-  DB_PORT=$(grep DB_PORT .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-  DB_DATABASE=$(grep DB_DATABASE .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-  DB_USERNAME=$(grep DB_USERNAME .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-  DB_PASSWORD=$(grep DB_PASSWORD .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
-
-  DB_URL="${DB_CONNECTION}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
-
-  echo "Opening ${DB_URL}"
-  open $DB_URL
-}
-
+###########################################################
 # Git
+###########################################################
 
-# Git aliases
-alias nah="git reset --hard && git clean -df" # clear changes since last commit
-alias reflog="git reflog show"                # show you the history of HEAD
-alias gs="git status"
+alias ga='git add'
+alias gaa='git add --all'
 alias gb="git branch"
-alias gc="git checkout"
+alias gba="git branch --all"
+alias gbd='git branch --delete'
+alias gs="git status"
+alias gch="git checkout"
+alias gc='git commit --verbose'
+alias gcb='git checkout -b'
 alias gl="git log --oneline --decorate --color"
+alias diff="git diff"
 alias amend="git add . && git commit --amend --no-edit"
 alias commit="git add . && git commit -m"
-alias diff="git diff"
-alias force="git push --force"
-alias nuke="git clean -df && git reset --hard"
-alias pop="git stash pop"
+alias nah="git reset --hard && git clean -df"
 alias pull="git pull"
 alias push="git push"
-alias resolve="git add . && git commit --no-edit"
-alias stash="git stash -u"
-alias unstage="git restore --staged ."
-alias wip="commit wip"
 
-# Work
-work_path="$HOME/Dev/neutrino"
-amp_path="$work_path/amp-dashboard"
-dash_path="$work_path/dash"
-dash_api_path="$dash_path/dash-api"
-dash_client_path="$dash_path/dash-client"
+###########################################################
+# General Laravel
+###########################################################
 
-alias workp="cd $work_path"
-alias ampp="cd $amp_path"
-alias dashp="cd $dash_path"
-alias dashapip="cd $dash_api_path"
-alias dashclientp="cd $dash_client_path"
-
-alias amp="pstorm $amp_path"
-alias dash="pstorm $dash_path"
-alias dashapi="pstorm $dash_api_path"
-alias dashclient="pstorm $dash_client_path"
-
-alias fresh="art migrate:fresh --seed"
+alias fresh="artisan migrate:fresh --seed"
+alias seed="artisan db:seed"
---
+++ neutrino_aliases.zsh
@@ -0,0 +1,137 @@
+###########################################################
+# NEUTRINO ALIASES AND FUNCTIONS
+# Feel free to add your own aliases and functions here.
+###########################################################
+
+# General
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias src="source $HOME/.zshrc"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+
+# Change directory to Neutrino
+alias cdneutrino="cd $NEUTRINO_DIR"
+
+###########################################################
+# Git
+###########################################################
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gb="git branch"
+alias gba="git branch --all"
+alias gbd='git branch --delete'
+alias gs="git status"
+alias gch="git checkout"
+alias gc='git commit --verbose'
+alias gcb='git checkout -b'
+alias gl="git log --oneline --decorate --color"
+alias diff="git diff"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias nah="git reset --hard && git clean -df"
+alias pull="git pull"
+alias push="git push"
+
+###########################################################
+# General Laravel
+###########################################################
+
+alias fresh="artisan migrate:fresh --seed"
+alias seed="artisan db:seed"
+
+###########################################################
+# AMP
+###########################################################
+# Paths
+amp_path="$NEUTRINO_DIR/amp-dashboard"
+
+# Open AMP repo in PhpStorm
+alias amp="pstorm $amp_path"
+
+# Change directory to AMP
+alias cdamp="cd $amp_path"
+
+###########################################################
+# DASH
+###########################################################
+# Paths
+dash_path="$NEUTRINO_DIR/dash"
+dash_api_path="$dash_path/dash-api"
+dash_client_path="$dash_path/dash-client"
+
+# Open DASH repos in PhpStorm
+alias dash="pstorm $dash_path"
+alias dashapi="pstorm $dash_api_path"
+alias dashclient="pstorm $dash_client_path"
+
+# Change directory to DASH repos
+alias cddash="cd $dash_path"
+alias cddashapi="cd $dash_api_path"
+alias cddashclient="cd $dash_client_path"
+
+###########################################################
+# Commands for all projects
+###########################################################
+# Run PHP commands in Docker containers
+function php() {
+ case $PWD in
+   $dash_api_path)
+     docker-compose --file ../docker-compose.yml exec api php "$@"
+     ;;
+   *)
+     echo "PHP command not found for this project."
+     ;;
+   esac
+}
+
+# Run Composer commands in Docker containers
+function composer() {
+ case $PWD in
+   $dash_api_path)
+     docker-compose --file ../docker-compose.yml exec api composer "$@"
+     docker cp $(docker-compose --file ../docker-compose.yml ps -q api):/var/www/html/vendor .
+     ;;
+   *)
+     echo "Composer command not found for this project."
+     ;;
+   esac
+}
+
+# Run Artisan commands in Docker containers
+function artisan() {
+  case $PWD in
+  $dash_api_path)
+    docker-compose --file ../docker-compose.yml exec api php artisan "$@"
+    ;;
+  *)
+    echo "Artisan command not found for this project."
+    ;;
+  esac
+}
+
+# Turn Xdebug on
+function xoff() {
+ case $PWD in
+   $dash_api_path)
+     sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=debug,develop/g" ../.docker/env/.env.api
+     docker-compose --file ../docker-compose.yml up -d api
+     ;;
+   *)
+     echo "Xdebug on command not found for this project."
+     ;;
+   esac
+}
+
+# Turn Xdebug off
+function xoff() {
+ case $PWD in
+   $dash_api_path)
+     sed -i "" "s/XDEBUG_MODE=.*/XDEBUG_MODE=off/g" ../.docker/env/.env.api
+     docker-compose --file ../docker-compose.yml up -d api
+     ;;
+   *)
+     echo "Xdebug off command not found for this project."
+     ;;
+   esac
+}
